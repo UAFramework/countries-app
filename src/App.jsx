50ae74b389c8c6680069440b8f56de5560c4f17a
P.F. <pavel.filkovskiy@gmail.com>
2024-02-10 22:41:52 +0000

    no Select Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,81 @@
 import { useState, useEffect } from 'react'
-import RegionSelect from './components/RegionSelect'
 import CountryList from './components/CountryList'
 import CountryDetails from './components/CountryDetails'
 import './App.css'
 
 function App() {
-  const [region, setRegion] = useState()
-  const [subregion, setSubregion] = useState()
+  const [selectedRegion, setSelectedRegion] = useState("")
+  const [selectedSubregion, setSelectedSubregion] = useState("")
   const [country, setCountry] = useState()
+  
+  const [regionsAndSubregions, setRegionsAndSubregions] = useState({})
+
+  const fetchRegionsAndSubregions = async () => {
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all?fields=region,subregion")
+      if (response.ok) {
+        const data = await response.json()
+        const result = data.reduce((acc, {region, subregion}) => {
+          if (!acc.hasOwnProperty(region)) {
+            acc[region] = []
+          }
+
+          if (subregion !== "" && !acc[region].includes(subregion)) {
+            acc[region].push(subregion)
+          }
+
+          return acc
+        }, {})
+
+        setRegionsAndSubregions(result)
+
+      } else {
+        throw new Error(`Got a bad response from the server: ${response.statusText}`)
+      }
+    } catch (error) {
+      alert("An error occurred while fetching data.\n" + 
+            "See console log for more details.")
+      
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchRegionsAndSubregions()
+  }, []);
+
+  const handleRegionChange = (event) => {
+    setSelectedRegion(event.target.value)
+    setSelectedSubregion("")
+    setCountry(undefined)
+  }
+
+  const handleSubregionChange = (event) => {
+    setSelectedSubregion(event.target.value)
+    setCountry(undefined)
+  }
 
   return (
     <>
-      {
-        <RegionSelect setRegion={setRegion} setSubregion={setSubregion} />
-      }
+      <div id="select-bar">
+        <select name="regions" onChange={handleRegionChange} value={selectedRegion}>
+          <option value="" disabled>Pick a region</option>
+          {
+            Object.keys(regionsAndSubregions)
+              .map(region => <option key={region} value={region}>{region}</option>)
+          }
+        </select>
+
+        <select name="subregions" onChange={handleSubregionChange} value={selectedSubregion}>
+          <option value="" disabled>Pick a subregion</option>
+          {
+            regionsAndSubregions[selectedRegion]
+              ?.map(subregion => <option key={subregion} value={subregion}>{subregion}</option>)
+          }
+      </select>
+      </div>
       <div id="data-container">
-        <CountryList region={region} subregion={subregion} setCountry={setCountry} />
+        <CountryList region={selectedRegion} subregion={selectedSubregion} setCountry={setCountry} />
         <CountryDetails country={country}/>
       </div>
     </>
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,3 +1,44 @@
+import { useState, useEffect } from "react"
+import './CountryDetails.css'
+
 export default function CountryDetails({country}) {
+
+  const [countryDetails, setCountryDetails] = useState()
   
+  const fetchCountryData = async (country) => {
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+      if (response.ok) {
+        const data = await response.json()
+        setCountryDetails(data[0])
+      } else {
+        throw new Error(`Got a bad response from the server: ${response.statusText}`)
+      }
+    } catch (error) {
+      alert("An error occurred while fetching data.\n" + 
+            "See console log for more details.")
+
+      console.error(error)
+    } 
+  }
+
+  useEffect(() => {
+    if (country === undefined || country === "") {
+      setCountryDetails(undefined)
+    } else {
+      fetchCountryData(country)
+    }
+  }, [country])
+
+  return (
+    <>
+    {
+      Boolean(countryDetails) &&
+        <div id="country-details">
+          <p>name: {countryDetails.name?.common}</p>
+          <p>flag: <img src={countryDetails.flags.png}></img></p>
+        </div>
+    }
+    </>
+  )
 }
